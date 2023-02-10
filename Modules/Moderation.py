a47732ee35d87762ef71b0497210c9da77a8f3f2
Ruslan Bashinskii <ruslan30403@gmail.com>
2023-02-10 21:57:01 +1000

    Улучшение команд модерации

--- Modules/Moderation.py
+++ Modules/Moderation.py
@@ -1,19 +1,68 @@
 from vkbottle.bot import Bot, BotLabeler, Message
 from vkbottle.dispatch.rules.base import CommandRule
+from typing import Tuple
+from typing import Union
 
 from Config import TOKEN
 
 bot = Bot(token=TOKEN)
 bl = BotLabeler()
 
+ban_type = {
+    'h': 'hour(s)',
+    'd': 'day(s)',
+    'm': 'month(s)',
+    'p': 'permanent'
+}
 
-@bl.chat_message(CommandRule("ban", ["!", "/"], 2))
-async def ban(message: Message):
-    users_info = await bot.api.users.get(message.from_id)
-    await message.reply(f'{users_info[0].first_name}, к сожалению, команда бана пока что находится в разработке')
+
+@bl.chat_message(CommandRule(Union['ban', ('Ban', 'бан', 'Бан')], ['!', '/'], 2))
+async def ban(message: Message, args: Tuple[str]):
+    if (message.reply_message is not None) and (message.reply_message.from_id != message.from_id):
+        users_info = await bot.api.users.get(message.from_id)
+        ban_user_info = await bot.api.users.get(message.reply_message.from_id)
+
+        await message.reply(f'{users_info[0].first_name}, к сожалению, команда бана пока что находится в разработке')
+
+        time = args[0]
+        time_type = ban_type[args[1]]
+
+        if args[1] == 'p':
+            time = ''
+        elif args[1] == 'm':
+            if int(time) < 0:
+                time = '1'
+            if int(time) > 12:
+                time = '12'
+        elif args[1] == 'd':
+            if int(time) < 0:
+                time = '1'
+            if int(time) > 31:
+                time = '31'
+        elif args[1] == 'h':
+            if int(time) < 0:
+                time = '1'
+            if int(time) > 24:
+                time = '24'
+        else:
+            time = '1'
+            time_type = ban_type['d']
+
+        await message.answer(f'Но если бы бан работал, то {ban_user_info[0].first_name} был бы забанен на {time} {time_type}')
+
+    elif message.reply_message.from_id == message.from_id:
+        users_info = await bot.api.users.get(message.from_id)
+        await message.reply(f'{users_info[0].first_name}, нельзя применить команду к себе.')
 
 
-@bl.chat_message(CommandRule("warn", ["!", "/"], 2))
+@bl.chat_message(CommandRule(Union['warn', ('Warn', 'варн', 'Варн')], ['!', '/'], 0))
 async def warn(message: Message):
-    users_info = await bot.api.users.get(message.from_id)
-    await message.reply(f'{users_info[0].first_name}, к сожалению, команда предупреждения пока что находится в разработке')
+    if (message.reply_message is not None) and (message.reply_message.from_id != message.from_id):
+        users_info = await bot.api.users.get(message.from_id)
+        warn_user_info = await bot.api.users.get(message.reply_message.from_id)
+        await message.reply(f'{users_info[0].first_name}, к сожалению, команда предупреждения пока что находится в разработке')
+        await message.answer(f'Но если бы предупреждение работало, то {warn_user_info[0].first_name} получил бы предупреждение')
+
+    elif message.reply_message.from_id == message.from_id:
+        users_info = await bot.api.users.get(message.from_id)
+        await message.reply(f'{users_info[0].first_name}, нельзя применить команду к себе.')
