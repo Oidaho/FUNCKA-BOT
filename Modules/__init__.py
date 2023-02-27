d9a371ade07394b63f0e1ac2535cc8bcdaf6929b
Ruslan Bashinskii <ruslan30403@gmail.com>
2023-02-27 21:02:02 +1000

    Handlers init

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,3 +1,3 @@
-from . import ForbiddenFilter, URLFilter, MessageQueueHandler, Moderation, ConversationRegistrator
+from . import ForbiddenFilter, MessageQueueHandler, Moderation, ConversationRegistrator, URLFilter
 
 labelers = [ConversationRegistrator.bl, MessageQueueHandler.bl, Moderation.bl, ForbiddenFilter.bl, URLFilter.bl]
